7a526e6d99894cda4ecb78d59a61081c58ef8d5e
paulflang <paul.lang@wolfson.ox.ac.uk>
2022-06-28 13:01:00 +0100

    initial commit

---
+++ test/runtests.jl
@@ -0,0 +1,3 @@
+using SafeTestsets, Test
+
+@safetestset "SBML Test Suite" begin include("sbmltestsuite.jl") end
---
+++ test/sbmltestsuite.jl
@@ -0,0 +1,14 @@
+using SBMLToolkitTestSuite
+using Test
+
+const case_ids = [1:2...]
+
+const logdir = joinpath(@__DIR__, "logs")
+ispath(logdir) && rm(logdir,recursive=true)
+mkdir(logdir)
+
+df = verify_all(case_ids)
+
+for i in 1:length(case_ids)
+    @test sum(Vector(df[i, ["expected_err", "res"]])) == 1
+end
