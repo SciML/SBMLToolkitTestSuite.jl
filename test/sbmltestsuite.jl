75e0986e6af0c9a4a95712086685e9e6641d6bac
paulflang <plang@deeporigin.com>
2024-07-17 13:05:03 +0200

    fix reporting of passed cases

--- test/sbmltestsuite.jl
+++ test/sbmltestsuite.jl
@@ -18,29 +18,25 @@ df = verify_all(case_ids, logdir)
 # Save current results
 current_results = Dict()
 for i in 1:length(case_ids)
-    current_results[string(case_ids[i])] = sum(Vector(df[i, ["expected_err", "res"]])) == 1
+    current_results[string(case_ids[i])] = df[i, "res"]
 end
 open(joinpath(logdir, "current_results.json"), "w") do io
     JSON.print(io, current_results)
 end
 
 # Compare current results with previous results and log the results
-let passed_cases = 0
-    total_cases = length(current_results)
-
-    for (case_id, result) in current_results
-        if haskey(prev_results, case_id)
-            @test result == prev_results[case_id]
-        end
-        if result
-            passed_cases += 1
-        end
+for (case_id, result) in current_results
+    if haskey(prev_results, case_id)
+        @test result == prev_results[case_id]
     end
+end
 
-    # Log the results
-    open(joinpath(logdir, "test_summary.log"), "w") do io
-        println(io, "Total cases: $total_cases")
-        println(io, "Passed cases: $passed_cases")
-        println(io, "Failed cases: $(total_cases - passed_cases)")
-    end
+# Log the results
+n_download_errors = sum(startswith.("Downloads.RequestError", df[:, "err"]))
+total_cases = length(case_ids) - n_download_errors
+passed_cases = sum(df[i, "res"])
+open(joinpath(logdir, "test_summary.log"), "w") do io
+    println(io, "Total cases: $total_cases")
+    println(io, "Passed cases: $passed_cases")
+    println(io, "Failed cases: $(total_cases - passed_cases)")
 end
